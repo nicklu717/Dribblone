8013a6d34c05ad44a8f361b16445d17419e1bc7c
陸瑋恩 <nicklu717@gmail.com>
2019-08-30 17:11:32 +0800

    Add movement-tracker view controller

---
+++ Dribble Training/MovementTracker/Controller/MovementTrackerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MovementTrackerViewController.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/8/30.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class MovementTrackerViewController: UIViewController {
+    
+}
---
+++ Dribble Training/MovementTracker/View/MovementTrackerView.swift
@@ -0,0 +1,13 @@
+//
+//  MovementTrackerView.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/8/30.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class MovementTrackerView: UIView {
+    
+}
