67d0a9bcd6cefe97969bd2b164992d8cbaaf14a2
陸瑋恩 <nicklu717@gmail.com>
2019-08-30 18:09:36 +0800

    Set capture session input

--- Dribble Training/MovementTracker/View/MovementTrackerView.swift
+++ Dribble Training/MovementTracker/View/MovementTrackerView.swift
@@ -7,10 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
-protocol MovementTrackerViewDelegate: AnyObject {}
+protocol MovementTrackerViewDelegate: AnyObject, AVCaptureVideoDataOutputSampleBufferDelegate {}
 
 class MovementTrackerView: UIView {
     
+    // MARK: - Property Declaration
+    
     weak var delegate: MovementTrackerViewDelegate?
+    
+    private let captureSession = AVCaptureSession()
+    
+    // MARK: - Private Method
+    
+    private func setUpCaptureSession() {
+        
+        // Set Session Input
+        
+        guard
+            let camera = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                   for: .video,
+                                                   position: .back),
+            let cameraInput = try? AVCaptureDeviceInput(device: camera)
+        else {
+            print("Couldn't Set Up Camera Input")
+            return
+        }
+        
+        captureSession.addInput(cameraInput)
+        
+//        // Set Session Output
+//
+//        let videoDataOutput = AVCaptureVideoDataOutput()
+//
+//        let videoDataOutputQueue = DispatchQueue(label: "Video Data Output")
+//
+//        videoDataOutput.setSampleBufferDelegate(self.delegate, queue: videoDataOutputQueue)
+//
+//        captureSession.addOutput(videoDataOutput)
+    }
 }
