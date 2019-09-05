bf0d5dcf632ed16ecf2472ba3d2987343c21d36f
陸瑋恩 <nicklu717@gmail.com>
2019-09-05 16:26:39 +0800

    Add local storage manager

---
+++ Dribble Training/Common/Model/StorageManager.swift
@@ -0,0 +1,33 @@
+//
+//  StorageManager.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/5.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import CoreData
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Dribble Training")
+        
+        container.loadPersistentStores { (_, error) in
+            
+            if let error = error {
+                fatalError("Unresolve Error: \(error), \(error.localizedDescription)")
+            }
+        }
+        
+        return container
+    }()
+    
+    var viewContext: NSManagedObjectContext {
+        
+        return persistentContainer.viewContext
+    }
+}
