965e882bbe78d5189a074abef4501f684134f398
陸瑋恩 <nicklu717@gmail.com>
2019-09-08 15:52:23 +0800

    Add training lobby view & controller

---
+++ Dribble Training/TrainingLobby/TrainingLobbyView.swift
@@ -0,0 +1,13 @@
+//
+//  TrainingLobbyView.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/8.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class TrainingLobbyView: UIView {
+    
+}
---
+++ Dribble Training/TrainingLobby/TrainingLobbyViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TrainingLobbyViewController.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/8.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class TrainingLobbyViewController: UIViewController {
+    
+    @IBOutlet var trainingLobbyView: TrainingLobbyView!
+}
