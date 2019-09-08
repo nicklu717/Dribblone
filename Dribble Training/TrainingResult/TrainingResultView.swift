0c5928df6e429e1011eb60f93bf2a51dbbbc2212
陸瑋恩 <nicklu717@gmail.com>
2019-09-08 20:47:37 +0800

    Add training result view & controller

---
+++ Dribble Training/TrainingResult/TrainingResultView.swift
@@ -0,0 +1,13 @@
+//
+//  TrainingResultView.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/8.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class TrainingResultView: UIView {
+    
+}
---
+++ Dribble Training/TrainingResult/TrainingResultViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TrainingResultViewController.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/8.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class TrainingResultViewController: UIViewController {
+    
+    @IBOutlet var trainingResultView: TrainingResultView!
+}
