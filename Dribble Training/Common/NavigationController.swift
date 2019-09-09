b8efa065e1470d4659d4db14bf5edc58ff032f1b
陸瑋恩 <nicklu717@gmail.com>
2019-09-09 23:33:00 +0800

    Set navigation controller supported interface orientations to portrait

---
+++ Dribble Training/Common/NavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationController.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/9.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
