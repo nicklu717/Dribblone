10ce9b64dcf33d125f07c9c583e3425dbbcadc03
陸瑋恩 <nicklu717@gmail.com>
2019-09-11 17:32:09 +0800

    Add profile view & controller

---
+++ Dribble Training/Profile/ProfileView.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileView.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/11.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+}
---
+++ Dribble Training/Profile/ProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewController.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/11.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet var profileView: ProfileView!
+}
