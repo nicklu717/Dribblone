9c915fd35c1669ffb85f3b0af3a290f03f4cd619
陸瑋恩 <nicklu717@gmail.com>
2019-09-12 16:03:24 +0800

    Add Firebase data struct

---
+++ Dribble Training/Firebase/FirebaseManager.swift
@@ -0,0 +1,16 @@
+//
+//  FirebaseManager.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/12.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import FirebaseFirestore
+
+class FirebaseManager {
+    
+    static let shared = FirebaseManager()
+    
+    
+}
---
+++ Dribble Training/Firebase/FirestoreStruct.swift
@@ -0,0 +1,56 @@
+//
+//  FirestoreStruct.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/12.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+//import Foundation
+
+struct Collection {
+    
+    static let member = "Member"
+    
+    static let trainingResults = "training_results"
+}
+
+struct Member: Codable {
+    
+    let id: String
+    
+    let email: String
+    
+    var followers: [String]
+    
+    var followings: [String]
+    
+    var trainingResults: [TrainingResult]
+    
+    var picture: String
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case id, email, followers, followings, picture
+        
+        case trainingResults = "training_results"
+    }
+}
+
+struct TrainingResult: Codable {
+    
+    let date: Double
+    
+    let mode: String
+    
+    let points: Int
+    
+    let videoLocalID: String
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case date, mode, points
+        
+        case videoLocalID = "video_local_id"
+    }
+}
