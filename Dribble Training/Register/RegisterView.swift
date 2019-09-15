96319f886245c696bac759d89d8132deb2d82df5
陸瑋恩 <nicklu717@gmail.com>
2019-09-15 17:43:14 +0800

    Add register view delegate

--- Dribble Training/Register/RegisterView.swift
+++ Dribble Training/Register/RegisterView.swift
@@ -8,7 +8,16 @@
 
 import UIKit
 
+protocol RegisterViewDelegate: AnyObject {
+    
+    func logIn(withEmail email: String, password: String)
+    
+    func signUp(withEmail email: String, password: String, id: String)
+}
+
 class RegisterView: UIView {
+    
+    weak var delegate: RegisterViewDelegate?
     
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
@@ -18,9 +27,20 @@ class RegisterView: UIView {
     @IBOutlet var signUpView: UIView!
     
     @IBAction func logIn() {
+        
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        delegate?.logIn(withEmail: email, password: password)
     }
     
     @IBAction func signUp() {
+        
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let id = idTextField.text ?? ""
+        
+        delegate?.signUp(withEmail: email, password: password, id: id)
     }
     
     @IBAction func switchStatus() {
