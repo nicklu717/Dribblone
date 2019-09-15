282fbcc720a4acfc64f2ac8b3a692de9e9e8c7f4
陸瑋恩 <nicklu717@gmail.com>
2019-09-15 17:16:36 +0800

    Add log-in view UI & IBOutlet

--- Dribble Training/LogIn/LogInView.swift
+++ Dribble Training/LogIn/LogInView.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class LogInView: UIView {
     
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var idLabel: UILabel!
+    @IBOutlet var idTextField: UITextField!
+    
+    @IBOutlet var logInButton: UIButton!
+    @IBOutlet var switchToSignUpButton: UIButton!
+    
+    @IBOutlet var signUpButton: UIButton!
+    @IBOutlet var switchToLogInButton: UIButton!
+    
+    var isLogIn: Bool = true
+
+    @IBAction func logIn() {
+    }
+    
+    @IBAction func signUp() {
+    }
+    
+    @IBAction func switchStatus() {
+    }
 }
