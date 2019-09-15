b0dc7abb7ebb150bfb17f429a2be7d44d3132e17
陸瑋恩 <nicklu717@gmail.com>
2019-09-15 16:28:38 +0800

    Add log-in view & controller template

---
+++ Dribble Training/LogIn/LogInView.swift
@@ -0,0 +1,13 @@
+//
+//  LogInView.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/15.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class LogInView: UIView {
+    
+}
---
+++ Dribble Training/LogIn/LogInViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LogInViewController.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/15.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    @IBOutlet var logInView: LogInView!
+}
