dc005a0bbead99b296cde743f7b52d3964369bb5
陸瑋恩 <nicklu717@gmail.com>
2019-09-21 18:09:27 +0800

    Add post wall view & controller

---
+++ Dribble Training/PostWall/PostWallView.swift
@@ -0,0 +1,18 @@
+//
+//  PostWallView.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/21.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class PostWallView: UIView {
+    
+    @IBOutlet var trainingResultPageView: UIView!
+    
+    var trainingResultPage: TrainingResultViewController!
+    
+    func setupTrainingResultPage() {}
+}
---
+++ Dribble Training/PostWall/PostWallViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PostWallViewController.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/21.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class PostWallViewController: UIViewController {
+    
+    @IBOutlet var postWallView: PostWallView!
+}
