a5c54c4a3d21fe998ef90b6300935f3775f72afc
陸瑋恩 <nicklu717@gmail.com>
2019-09-24 19:06:26 +0800

    Add settings table view cell

---
+++ Dribble Training/Profile/Settings/SettingsTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewCell.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/24.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+    
+}
