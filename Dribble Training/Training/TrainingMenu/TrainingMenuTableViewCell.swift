15e2545b3ea4d0b3e61fee417c4aa86d727b950b
陸瑋恩 <nicklu717@gmail.com>
2019-09-25 13:34:21 +0800

    Add training menu table view cell

---
+++ Dribble Training/Training/TrainingMenu/TrainingMenuTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrainingMenuTableViewCell.swift
+//  Dribble Training
+//
+//  Created by 陸瑋恩 on 2019/9/25.
+//  Copyright © 2019 陸瑋恩. All rights reserved.
+//
+
+import UIKit
+
+class TrainingMenuTableViewCell: UITableViewCell {
+    
+}
